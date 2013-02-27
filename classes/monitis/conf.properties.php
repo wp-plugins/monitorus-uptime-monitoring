944781aaa7235261ba700154bd174669cd6eb5d5
Monitor.us <Monitor.us@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-27 07:04:02 +0000

    removed comments from conf properties
    git-svn-id: https://plugins.svn.wordpress.org/monitorus-uptime-monitoring/trunk@673925 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/monitis/conf.properties.php
+++ classes/monitis/conf.properties.php
@@ -7,10 +7,4 @@ global $serverUrl;
 global $serverUrlcustom;
 $serverUrl = "http://www.monitor.us/api";
 $serverUrlcustom = "http://www.monitor.us/customMonitorApi";
-
-//$apiKey = "4NI0R6JIRQ05RMISP4AGU8NF9";
-//$secretKey = "4UJ7BNQS27O0NNOIA74JBRIMF";
-/*
-$apiKey = "5AA21UT08C56OJHBKL";
-$secretKey = "4S0TUPJLN5P3FRFHPCOATQ4H5N";*/
 ?>
